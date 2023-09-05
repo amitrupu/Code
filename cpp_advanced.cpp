483f390a4825878beab355eacba909ecf49e71b9
Amit Roy <amitroy@us01odcvde31570.internal.synopsys.com>
2023-09-05 05:21:01 -0700

    a

--- cpp_advanced.cpp
+++ cpp_advanced.cpp
@@ -2,22 +2,24 @@
 #include <functional>
 using namespace std;
 
+// C++11 features
+
 // lamda
 void lambda_test() {
-	int a = 10, b = 20;
-	auto max = [] (int &a, int &b)->int& {
-		return (a>b)? a: b;
-	};
-	[&](function<int&(int&,int&)> f) {
-		cout << f(a,b) << endl;
-	}(max);
-	[&](function<int&(int&,int&)> f) {
-		f(a,b) = 0;
-	}(max);
-	cout << a << " " << b << endl;
+    int a = 10, b = 20;
+    auto max = [] (int &a, int &b)->int& {
+        return (a>b)? a: b;
+    };
+    [&](function<int&(int&,int&)> f) {
+        cout << f(a,b) << endl;
+    }(max);
+    [&](function<int&(int&,int&)> f) {
+        f(a,b) = 0;
+    }(max);
+    cout << a << " " << b << endl;
 }
 
 int main() {
-	lambda_test();
-	return 0;
+    lambda_test();
+    return 0;
 }
